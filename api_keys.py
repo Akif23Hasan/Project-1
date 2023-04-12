e7e4810abe51d93f88b777711540c083830205ed
Akif Hasan <123386740+Akif23Hasan@users.noreply.github.com>
2023-04-12 21:19:24 +1000

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "25cbe733b5818eb3fec3338d511bcbf5"
-
-# Geoapify API Key
-geoapify_key = "9620974308c6477c8f3e31f84708640f"
-
-#"540131cceac6478cae3786ee7a91bc34"
